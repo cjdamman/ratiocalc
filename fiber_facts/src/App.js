7e739d31ad64c940674b22ca6be000c365df1c99
scotchwsplenda <lockwood.oliver@gmail.com>
2022-09-06 12:47:46 -0700

    lil work

---
+++ fiber_facts/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+  const [nutrition, setNutrition] = useState([]);
+
+  async function fetchNutritionHandler() {
+    const response = await fetch('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=dvHXrelaUtReddQBiicnlA0lFCApcAjTPgv7I7mm&query=Cheddar%20Cheese');
+    const data = await response.json();
+    const transformedNutrition = await data.foods.map((nutritionData) => {
+      return {
+        openingText: nutritionData.description,
+        title: nutritionData.brandOwner,
+        id: nutritionData.fdcid
+      }
+    })
+    // console.log(transformedNutrition)
+    console.log(data.foods)
+    setNutrition(transformedNutrition)
+
+
+
+
+
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchNutritionHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={nutrition} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
