aecb4556fb73cc6267de9a37e7aa4a233efa7e85
Luke Walker <luke@yubico.com>
2022-12-13 14:54:43 -0800

    fix spelling, add unit names

--- fiber_facts/src/App.js
+++ fiber_facts/src/App.js
@@ -14,44 +14,47 @@ function App() {
     console.log(data.foods)
     const transformedNutrition = await data.foods.map((nutritionData) => {
       // i want to loop through the values in nuts and create an object with a vernacular name as the key and foodNutrients.value as the key, and if nuts isn't in there just make the value 0
-    const nuts = ["Fiber, total dietary", "Carbohydrate, by difference", "Sodium, Na", "Total lipid (fat)",  "Fatty acids, total monounsaturated", "Fatty acids, total polyunsaturated"]
-    const filter = nutritionData.foodNutrients.filter(i => nuts.includes(i.nutrientName))
-    const funk = filter.map(x => x.nutrientName)
-    const funk1 = filter.map(x => x.value)
+    //const nuts = ["Fiber, total dietary", "Carbohydrate, by difference", "Sodium, Na", "Total lipid (fat)",  "Fatty acids, total monounsaturated", "Fatty acids, total polyunsaturated"]
+    //const filter = nutritionData.foodNutrients.filter(i => nuts.includes(i.nutrientName))
+    //const funk = filter.map(x => x.nutrientName)
+    //const funk1 = filter.map(x => x.value)
     
 
     return {
         openingText: nutritionData.description,
         title: nutritionData.brandOwner,
         id: nutritionData.fdcid,
-        nutrients: [...nutritionData.foodNutrients.map(nut => [nut.nutrientName +': '+ nut.value+', '])],
+        nutrients: [...nutritionData.foodNutrients.map(nut => [nut.nutrientName + ': ' + nut.value + ', '])],
         // nutrients:  [...nutritionData.foodNutrients.map(nut => [nut.nutrientName , nut.value])][0][0],
         nuts2: {
-        fiber:  [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fiber, total dietary"), 1][0].value ,
-        carbs: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Carbohydrate, by difference"), 1][0].value,
-        sodium: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Sodium, Na"), 1][0].value, 
-        potassium: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Potassium, K"), 1][0].value,
-        tot_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Total lipid (fat)"), 1][0].value,
-        sat_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total saturated"), 1][0].value,
-        trans_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total trans"), 1][0].value,
-        mono_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total monounsaturated"), 1][0].value,
-        poly_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total polyunsaturated"), 1][0].value
-      }
-        
-        ,
+          fiber:  [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fiber, total dietary"), 1][0].value,
+          fiberUnitName:  [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fiber, total dietary"), 1][0].unitName,
+          carbs: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Carbohydrate, by difference"), 1][0].value,
+          carbsUnitName: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Carbohydrate, by difference"), 1][0].unitName,
+          sodium: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Sodium, Na"), 1][0].value, 
+          sodiumUnitName: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Sodium, Na"), 1][0].unitName, 
+          potassium: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Potassium, K"), 1][0].value,
+          potassiumUnitName: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Potassium, K"), 1][0].unitName,
+          tot_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Total lipid (fat)"), 1][0].value,
+          tot_fatUnitName: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Total lipid (fat)"), 1][0].unitName,
+          sat_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total saturated"), 1][0].value,
+          sat_fatUniteName: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total saturated"), 1][0].unitName,
+          trans_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total trans"), 1][0].value,
+          trans_fatUnitName: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total trans"), 1][0].unitName,
+          mono_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total monounsaturated"), 1][0].value,
+          mono_fatUnitName: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total monounsaturated"), 1][0].unitName,
+          poly_fat: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total polyunsaturated"), 1][0].value,
+          poly_fatUnitName: [...nutritionData.foodNutrients.filter(nut  => nut.nutrientName==="Fatty acids, total polyunsaturated"), 1][0].unitName
+        },
         filtered: [...nutritionData.foodNutrients.map(nut => [[nut.nutrientName , nut.value]])],
         filtered2: [...nutritionData.foodNutrients.map(nut => { return [nut.nutrientName , nut.value]})],
         // filtered3: nutritionData.foodNutrients.filter(i => nuts.includes(i.nutrientName))
-      
       }
-       
     })
     
     console.log(transformedNutrition)
-    setNutrition(transformedNutrition)}
-
-
-
+    setNutrition(transformedNutrition)
+  }
 
   const searchFoodHandler = (event) => {
     console.log(inputs)
--- fiber_facts/src/components/Movie.js
+++ fiber_facts/src/components/Movie.js
@@ -10,13 +10,13 @@ const Movie = (props) => {
     <li className={classes.movie}>
       <h1>{props.z}</h1>
       <table className={classes.tabley} >
-        <th className={classes.tabley} >{`Carbs ${props.carbs} / Fiber ${props.fiber}`} </th>
-        <th className={classes.tabley} >{`(TFat ${props.tot_fat} / UnsatTat ${Math.round((props.tot_fat - (props.trans_fat + props.sat_fat))*100)/100}) * 4`} </th>
+        <th className={classes.tabley} >{`Carbs ${props.carbs}${props.carbsUnitName} / Fiber ${props.fiber}${props.fiberUnitName} `} </th>
+        <th className={classes.tabley} >{`(Fat ${props.tot_fat}${props.tot_fatUnitName} / UnsatFat ${Math.round((props.tot_fat - (props.trans_fat + props.sat_fat))*100)/100}${props.tot_fatUnitName}) * 4`} </th>
         <tr>
           <td className={(FibRatio) > 10 ? classes.red : (FibRatio) > 5 ? classes.yellow : (FibRatio) < 1 ? classes.purple : classes.green}> {Math.round(FibRatio)}</td>
           <td className={(FatRatio) > 10 ? classes.red : (FatRatio) > 5 ? classes.yellow : (FatRatio) < 1 ? classes.purple : classes.green}> {Math.round(FatRatio)}</td>
         </tr>
-          <th className={classes.tabley} > {`(Salt ${props.salt} / Potassium ${props.k || 0}) * 5`} </th>
+          <th className={classes.tabley} > {`(Salt ${props.salt}${props.saltUnitName} / Potassium ${props.k || 0}${props.kUnitName}) * 5`} </th>
           <th className={classes.tabley}> Recommendation </th>
         <tr>
          <td className={(SaltRatio) > 10 ? classes.red : (SaltRatio) > 5 ? classes.yellow : classes.green}> {Math.round(SaltRatio)}</td>
